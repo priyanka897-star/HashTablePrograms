afa4fa39d6fe995f0440a8cd042310a424354a9f
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-10-23 00:04:58 +0530

    Refactored:Indentation Correction

--- hashmapproblems/HashMapTest.java
+++ hashmapproblems/HashMapTest.java
@@ -7,23 +7,21 @@ public class HashMapTest {
 	
 	public static void gitvenASentence_WhenWordAreAddToList_ShoudReturnWordFrequency () {
 		String sentence = "Paranoids are not paranoid because they are paranoid but because "	
-				           +"they keep putting themselves deliberately into paranoid avoidable situations";
+				  +"they keep putting themselves deliberately into paranoid avoidable situations";
 		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
 		String[] words = sentence.toLowerCase().split(" ");
 		for (String word : words) {
 			Integer value = myHashMap.get(word);
 			if (value == null)
 				value = 1;
-			
 			else
 				value = value + 1;
 			myHashMap.add(word, value);
 		}
-	    System.out.println(myHashMap);
-	    myHashMap.removeKey("avoidable");
-	    System.out.println("Hash Map after deleting avoidable");
+	        System.out.println(myHashMap);
+	        myHashMap.removeKey("avoidable");
+	        System.out.println("Hash Map after deleting avoidable");
 		System.out.println(myHashMap);
-		
 		}
 }
 
